11005a8ff8adfc48f8c4da14d7ddebbec4439d21
Valentine Omonya <valomosh254@gmail.com>
2024-09-23 21:31:12 +0300

    Added clerk sign-in sign-out

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import {  ClerkProvider} from '@clerk/nextjs'
 import "./globals.css";
 
 const geistSans = localFont({
@@ -24,6 +25,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
+
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -31,5 +34,7 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+    </ClerkProvider>
+
   );
 }
