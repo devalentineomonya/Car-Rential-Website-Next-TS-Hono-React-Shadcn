4a4b285104702b37f97b3cad664ab84a705c722a
Valentine Omonya <valomosh254@gmail.com>
2024-11-23 23:28:53 +0300

    Initialized admin and user dashboards

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,27 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(['/admin(.*)', '/user(.*)']);
-export default clerkMiddleware((auth, request) => {
-  if (isProtectedRoute(request)) {
-    auth().protect();
-  }
-}, { debug: true });
+const isProtectedRoute = createRouteMatcher(["/admin(.*)", "/user(.*)"]);
+export default clerkMiddleware(
+  (auth, request) => {
+    if (isProtectedRoute(request)) {
+      auth().protect();
+      const url = new URL(request.url);
+      const path = url.pathname;
+      if (path === "/user") {
+        return NextResponse.redirect(new URL("/user/dashboard", request.url));
+      }
+      if (path === "/admin") {
+        return NextResponse.redirect(new URL("/admin/dashboard", request.url));
+      }
+    }
+  },
+  { debug: true }
+);
 
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
   ],
 };
