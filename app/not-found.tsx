7d67c9791ab3f14eb063c653fffa06c3381d413e
Valentine Omonya <valomosh254@gmail.com>
2025-02-27 06:58:10 +0300

    Added image priority

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,13 +6,23 @@ import notFoundImage from "@/public/images/notFound.png";
 
 const NotFound = () => {
   return (
-    <div className="flex-1 flex flex-col items-center justify-center pr-2 mt-24 max-lg:px-2 min-h-[75dvh]">
+    <div className="flex-1 flex flex-col items-center justify-center pr-2 mt-24 max-lg:px-2 min-h-[60dvh]">
       <h1 className="text-2xl font-bold">Are you Lost?</h1>
-      <Image src={notFoundImage} alt="404" width={500} height={500} />
+      <Image
+        src={notFoundImage}
+        alt="404"
+        width={500}
+        height={500}
+        priority
+        className="my-4"
+      />
       <p className="text-gray-500">
         The page you are looking for does not exist.
       </p>
-      <Link href="/" className="text-blue-500">
+      <Link
+        href="/"
+        className="inline-block py-2 bg-primary text-white px-4 rounded-md mt-4"
+      >
         Go back to the home page
       </Link>
     </div>
