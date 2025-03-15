2177b01f7f3af1d0fd8732821d363a03885c326c
Valentine Omonya <valomosh254@gmail.com>
2025-03-15 23:24:08 +0300

    Modified file.ts

--- lib/file.ts
+++ lib/file.ts
@@ -1,6 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
 
-// Configure Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
