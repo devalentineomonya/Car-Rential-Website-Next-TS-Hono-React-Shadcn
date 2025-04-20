2d3c40ddff859dc2c55cb25e30b5016f306a7aed
Valentine Omonya <valomosh254@gmail.com>
2025-04-20 23:24:48 +0300

    feat(UI): add SortByController for dynamic car sorting

---
+++ screens/cars/components/SortByController.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const SortByController = () => {
+  return (
+    <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Sort By" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="pricePerDay_asc">Price: Low to High</SelectItem>
+        <SelectItem value="pricePerDay_desc">Price: High to Low</SelectItem>
+        <SelectItem value="mileage_asc">Mileage: Low to High</SelectItem>
+        <SelectItem value="dateManufactured_desc">Newest Vehicles</SelectItem>
+        <SelectItem value="make_asc">Make: A to Z</SelectItem>
+        <SelectItem value="createdAt_desc">Newest Listings</SelectItem>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default SortByController;
