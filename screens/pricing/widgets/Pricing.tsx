b7d72b900440a9bdfccd2e14a64e5b87fd5ad3c5
Valentine Omonya <valomosh254@gmail.com>
2025-04-20 23:55:42 +0300

    refactor(pricing): streamline Pricing widget logic and cleanup props

--- screens/pricing/widgets/Pricing.tsx
+++ screens/pricing/widgets/Pricing.tsx
@@ -1,28 +1,48 @@
 import MainLayout from "@/components/common/layouts/MainLayout";
-
 import DesktopPricing from "../components/DesktopPricing";
 import MobilePricing from "../components/MobilePricing";
 import PricingCard from "../components/PricingCard";
+import { rentalPlans, rentalFeatures, rentalPerks } from "./pricingData";
 
-import { plans, features, perks } from "./pricingData";
-
-export default function Example() {
+export default function PricingPage() {
   return (
     <MainLayout>
       <div className="relative mt-12 px-2">
-        <div
+        {/* <div
           aria-hidden="true"
           className="hidden absolute top-4 bottom-6 left-0 right-0 inset-0 bg-gray-700 rounded-tl-lg rounded-tr-lg lg:block"
-        />
+        /> */}
 
-        <div className="relative max-lg:space-y-6 lg:grid lg:grid-cols-3">
-          {plans.map((plan) => (
-            <PricingCard key={plan.title} plan={plan} />
+        {/* Pricing Cards Section */}
+        <div className="relative max-lg:space-y-6 lg:grid lg:grid-cols-3 lg:gap-8">
+          {rentalPlans.map((plan) => (
+            <PricingCard
+              key={plan.title}
+              plan={{
+                ...plan,
+                priceLabel: `/day`,
+                periodLabel: "Weekly discount",
+                pricePeriod: plan.priceWeekly,
+              }}
+            />
           ))}
         </div>
+
+        {/* Pricing Comparison Tables */}
+        <div className="mt-16">
+          <MobilePricing
+            plans={rentalPlans}
+            features={rentalFeatures}
+            perks={rentalPerks}
+          />
+
+          <DesktopPricing
+            plans={rentalPlans}
+            features={rentalFeatures}
+            perks={rentalPerks}
+          />
+        </div>
       </div>
-      <MobilePricing plans={plans} features={features} perks={perks} />
-      <DesktopPricing plans={plans} features={features} perks={perks} />
     </MainLayout>
   );
 }
