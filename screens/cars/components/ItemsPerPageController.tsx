a676c5a728521389b923ddb84f2af8abb392d8dd
Valentine Omonya <valomosh254@gmail.com>
2025-04-20 23:24:46 +0300

    feat(UI): add ItemsPerPageController for car list pagination

---
+++ screens/cars/components/ItemsPerPageController.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const ItemsPerPageController = () => {
+  return (
+    <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Show Items" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="8">Show 8</SelectItem>
+        <SelectItem value="12">Show 12</SelectItem>
+        <SelectItem value="16">Show 16</SelectItem>
+        <SelectItem value="20">Show 20</SelectItem>
+        <SelectItem value="24">Show 24</SelectItem>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default ItemsPerPageController;
