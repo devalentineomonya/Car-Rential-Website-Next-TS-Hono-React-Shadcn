c3f3137d347b612656ec3b0d8cbc165088facc05
Valentine Omonya <valomosh254@gmail.com>
2025-04-30 19:33:29 +0300

    Modified responsivity for car cards

--- screens/cars/widgets/CarList.tsx
+++ screens/cars/widgets/CarList.tsx
@@ -21,12 +21,12 @@ const CarList = () => {
     <MainLayout>
       <div className="flex-1 pr-2 mt-24 mb-12 max-lg:px-2"></div>
       <div className="max-lg:px-2 flex gap-4 pb-8">
-        <div className="w-60 shrink-0 max-sm:hidden">
+        <div className="w-60 shrink-0 max-md:hidden">
           <div className="sticky top-20">
             <CarFilter onFilterChange={setFilters} />
           </div>
         </div>
-        <div>
+        <div className="w-full">
           <div className="w-full flex items-center justify-between my-4">
             <div className="flex items-center gap-x-3">
               <Button variant="outline" size="icon">
@@ -39,7 +39,7 @@ const CarList = () => {
               <OrientationController />
             </div>
           </div>
-          <div className="flex-1 sm:grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 max-h-fit">
+          <div className="flex-1 sm:grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-3 max-h-fit w-full">
             {isPending
               ? Array(8)
                   .fill(0)
